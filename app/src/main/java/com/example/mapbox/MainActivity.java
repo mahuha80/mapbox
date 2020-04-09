3260b1abc740c9a2ca05319d6bbe4ccdb918d71c
vinhnt_ <thanhvinh2ker@gmail.com>
2020-04-09 09:59:38 +0700

    format

--- app/src/main/java/com/example/mapbox/MainActivity.java
+++ app/src/main/java/com/example/mapbox/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.mapbox;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mapbox.android.gestures.Utils;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.geometry.LatLngQuad;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -28,6 +44,11 @@ import com.mapbox.mapboxsdk.plugins.annotation.OnSymbolLongClickListener;
 import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
+import com.mapbox.mapboxsdk.plugins.building.BuildingPlugin;
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerView;
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager;
+import com.mapbox.mapboxsdk.snapshotter.MapSnapshot;
+import com.mapbox.mapboxsdk.snapshotter.MapSnapshotter;
 import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.RasterLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions;
@@ -39,17 +60,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static android.app.PendingIntent.getActivity;
 import static com.mapbox.mapboxsdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_VIEWPORT;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    MapView mapView;
-    CircleManager circleManager;
-    SymbolManager symbolManager;
-    Symbol symbol;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener, View.OnClickListener {
     private static final String ID_IMAGE_SOURCE = "animated_image_source";
     private static final String ID_IMAGE_LAYER = "animated_image_layer";
     private final Random random = new Random();
-    private static final String ID_ICON_AIRPORT = "airport";
+    private BuildingPlugin buildingPlugin;
+    private MapView mapView;
+    private MarkerView markerView;
+    private MarkerViewManager markerViewManager;
+    private MapboxMap mapboxMap;
+    private NotificationManager notificationManager;
+    private MapSnapshotter mapSnapshotter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,84 +83,172 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-
-    }
-
-    private LatLng createRandomLatLng() {
-        return new LatLng((random.nextDouble() * -180.0) + 90.0,
-                (random.nextDouble() * -360.0) + 180.0);
     }
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        MainActivity.this.mapboxMap = mapboxMap;
         mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
-                LatLngQuad quad = new LatLngQuad(new LatLng(25.7836, -80.11725),
-                        new LatLng(25.783548, -80.1397431334),
-                        new LatLng(25.7680, -80.13964),
-                        new LatLng(25.76795, -80.11725));
-                style.addSource(new ImageSource(ID_IMAGE_SOURCE, quad, R.drawable.mac));
-                style.addLayer(new RasterLayer(ID_IMAGE_LAYER, ID_IMAGE_SOURCE));
-                CameraPosition position = new CameraPosition.Builder().target(new LatLng(25.76795, -80.11725)).zoom(10).tilt(20).build();
-                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 1000);
+                mapboxMap.addOnMapClickListener(MainActivity.this);
+                markerViewManager = new MarkerViewManager(mapView, mapboxMap);
+                buildingPlugin3d(style);
+                addView();
+                addImage(style);
             }
         });
-
     }
 
-//    @Override
-//    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-//        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-//            @Override
-//            public void onStyleLoaded(@NonNull Style style) {
-//                GeoJsonOptions geoJsonOptions = new GeoJsonOptions().withTolerance(0.4f);
-//                symbolManager = new SymbolManager(mapView, mapboxMap, style, null, geoJsonOptions);
-//                circleManager = new CircleManager(mapView, mapboxMap, style);
-//                circleManager.addClickListener(new OnCircleClickListener() {
-//                    @Override
-//                    public void onAnnotationClick(Circle circle) {
-//                        Toast.makeText(MainActivity.this, "here" + circle.getId(), Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//                circleManager.addLongClickListener(new OnCircleLongClickListener() {
-//                    @Override
-//                    public void onAnnotationLongClick(Circle circle) {
-//                        Toast.makeText(MainActivity.this, circle.getId() + "", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
-//                // create a fixed circle
-//                CircleOptions circleOptions = new CircleOptions()
-//                        .withLatLng(new LatLng(6.687337, 0.381457))
-//                        .withCircleColor(ColorUtils.colorToRgbaString(Color.YELLOW))
-//                        .withCircleRadius(12f)
-//                        .withDraggable(true);
-//                circleManager.create(circleOptions);
-//                // random add circles across the globe
-//                List<CircleOptions> circleOptionsList = new ArrayList<>();
-//                for (int i = 0; i < 20; i++) {
-//                    int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
-//                    circleOptionsList.add(new CircleOptions()
-//                            .withLatLng(createRandomLatLng())
-//                            .withCircleColor(ColorUtils.colorToRgbaString(color))
-//                            .withCircleRadius(8f)
-//                            .withDraggable(true)
-//                    );
-//                }
-//                circleManager.create(circleOptionsList);
-//
-//                SymbolOptions symbolOptions = new SymbolOptions()
-//                        .withLatLng(new LatLng(6.687337, 0.381457))
-//                        .withIconImage(ID_ICON_AIRPORT)
-//                        .withIconSize(1.3f)
-//                        .withSymbolSortKey(10.0f)
-//                        .withDraggable(true);
-//                symbol = symbolManager.create(symbolOptions);
-//            }
-//        });
 
+    private void addImage(Style style) {
+        mapboxMap.addMarker(new MarkerOptions().position(new LatLng(20.996280, 105.826810))).setTitle("PiraGo");
+        //add Image
+        LatLngQuad quad = new LatLngQuad(new LatLng(25.7836, -80.11725),
+                new LatLng(25.783548, -80.1397431334),
+                new LatLng(25.7680, -80.13964),
+                new LatLng(25.76795, -80.11725));
+        style.addSource(new ImageSource(ID_IMAGE_SOURCE, quad, R.drawable.logo));
+        style.addLayer(new RasterLayer(ID_IMAGE_LAYER, ID_IMAGE_SOURCE));
+    }
+
+    private void addView() {
+        View customView = LayoutInflater.from(MainActivity.this).inflate(R.layout.marker_view_bubble, null);
+        customView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        Button btn = customView.findViewById(R.id.btn);
+        btn.setText("PiraGo");
+        btn.setTextColor(getColor(R.color.colorPrimaryDark));
+        Button btn1 = customView.findViewById(R.id.btn1);
+        btn1.setText("Image");
+        btn1.setTextColor(getColor(R.color.colorPrimaryDark));
+        markerView = new MarkerView(new LatLng(20.996280, 105.826810), customView);
+        markerViewManager.addMarker(markerView);
+        btn.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+    }
+
+    private void buildingPlugin3d(Style style) {
+        //building plugin
+        buildingPlugin = new BuildingPlugin(mapView, mapboxMap, style);
+        buildingPlugin.setMinZoomLevel(15f);
+        buildingPlugin.setVisibility(true);
+    }
+
+    private void startSnapShot(final LatLngBounds latLngBounds, final int height, final int width) {
+        mapboxMap.getStyle(new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                if (mapSnapshotter == null) {
+                    MapSnapshotter.Options options =
+                            new MapSnapshotter.Options(width, height)
+                                    .withCameraPosition(mapboxMap.getCameraPosition())
+                                    .withStyle(style.getUri())
+                                    .withRegion(latLngBounds);
+
+                    mapSnapshotter = new MapSnapshotter(MainActivity.this, options);
+                } else {
+                    mapSnapshotter.setSize(width, height);
+                    mapSnapshotter.setRegion(latLngBounds);
+                }
+                mapSnapshotter.start(new MapSnapshotter.SnapshotReadyCallback() {
+                    @Override
+                    public void onSnapshotReady(MapSnapshot snapshot) {
+                        createNotification(snapshot.getBitmap());
+                    }
+                });
+            }
+        });
+    }
+
+    private void createNotification(Bitmap bitmap) {
+        final int notifyId = 1002;
+        String id = "channel_id";
+        if (notificationManager == null) {
+            notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null) {
+                NotificationChannel notificationChannel = notificationManager.getNotificationChannel(id);
+                if (notificationChannel == null) {
+                    notificationChannel = new NotificationChannel(id, "channel_name", NotificationManager.IMPORTANCE_HIGH);
+                    notificationChannel.setDescription("channel_description");
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+            }
+        }
+        Intent intent = new Intent(this, MainActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, id)
+                .setContentTitle("content")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(("title"))
+                .setContentText(("description"))
+                .setContentIntent(getActivity(this, 0, intent, 0))
+                .setLargeIcon(bitmap);
+        Notification notification = builder.build();
+        notificationManager.notify(notifyId, notification);
+    }
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        startSnapShot(mapboxMap.getProjection().getVisibleRegion().latLngBounds, mapView.getMeasuredHeight(), mapView.getMeasuredWidth());
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(20.996280, 105.826810)).zoom(10).tilt(20).build();
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                break;
+            case R.id.btn1:
+                CameraPosition cameraPosition1 = new CameraPosition.Builder().target(new LatLng(25.783548, -80.1397431334)).zoom(10).tilt(20).build();
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition1));
+                break;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
 }
-
-
-
