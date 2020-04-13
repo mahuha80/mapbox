e593e3b3d7e4edd5beb4be6d4c1f4aae84af62dd
vinhnt_ <thanhvinh2ker@gmail.com>
2020-04-13 09:55:20 +0700

    distance

--- app/src/main/java/com/example/mapbox/MainActivity.java
+++ app/src/main/java/com/example/mapbox/MainActivity.java
@@ -2,212 +2,190 @@ package com.example.mapbox;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
+import android.annotation.SuppressLint;
 import android.graphics.Color;
-import android.os.Build;
+import android.location.Location;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.mapbox.android.gestures.Utils;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.geometry.LatLngBounds;
-import com.mapbox.mapboxsdk.geometry.LatLngQuad;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.plugins.annotation.Circle;
-import com.mapbox.mapboxsdk.plugins.annotation.CircleManager;
-import com.mapbox.mapboxsdk.plugins.annotation.CircleOptions;
-import com.mapbox.mapboxsdk.plugins.annotation.OnCircleClickListener;
-import com.mapbox.mapboxsdk.plugins.annotation.OnCircleLongClickListener;
-import com.mapbox.mapboxsdk.plugins.annotation.OnSymbolClickListener;
-import com.mapbox.mapboxsdk.plugins.annotation.OnSymbolLongClickListener;
-import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
-import com.mapbox.mapboxsdk.plugins.building.BuildingPlugin;
-import com.mapbox.mapboxsdk.plugins.markerview.MarkerView;
-import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager;
-import com.mapbox.mapboxsdk.snapshotter.MapSnapshot;
-import com.mapbox.mapboxsdk.snapshotter.MapSnapshotter;
-import com.mapbox.mapboxsdk.style.layers.Property;
-import com.mapbox.mapboxsdk.style.layers.RasterLayer;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions;
-import com.mapbox.mapboxsdk.style.sources.ImageSource;
-import com.mapbox.mapboxsdk.utils.ColorUtils;
+import com.mapbox.mapboxsdk.style.layers.CircleLayer;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.turf.TurfMeasurement;
 
-import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.Locale;
 
-import static android.app.PendingIntent.getActivity;
-import static com.mapbox.mapboxsdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_VIEWPORT;
+import static com.mapbox.mapboxsdk.style.layers.Property.LINE_JOIN_ROUND;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineJoin;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
+import static com.mapbox.turf.TurfConstants.UNIT_METERS;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener, View.OnClickListener {
-    private static final String ID_IMAGE_SOURCE = "animated_image_source";
-    private static final String ID_IMAGE_LAYER = "animated_image_layer";
-    private final Random random = new Random();
-    private BuildingPlugin buildingPlugin;
+public class MainActivity extends AppCompatActivity implements
+        PermissionsListener, MapboxMap.OnCameraMoveListener {
+
+    private static final String DISTANCE_SOURCE_ID = "DISTANCE_SOURCE_ID";
+    private static final String DISTANCE_LINE_LAYER_ID = "DISTANCE_LINE_LAYER_ID";
+    private static final int LINE_COLOR = Color.RED;
+    private static final float LINE_WIDTH = 2f;
+
+    private PermissionsManager permissionsManager;
+    private List<Point> pointList;
     private MapView mapView;
-    private MarkerView markerView;
-    private MarkerViewManager markerViewManager;
     private MapboxMap mapboxMap;
-    private NotificationManager notificationManager;
-    private MapSnapshotter mapSnapshotter;
+    private TextView distanceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, ("pk.eyJ1IjoidmluaG5ndXllbjAwMTEwMSIsImEiOiJjazhtdHR3amkwZm1wM21uc2ptdHVueDJ2In0.ihDe_tGKr2ui9tM7MBXJbw"));
+        Mapbox.getInstance(this, getString(R.string.ACCESS_TOKEN));
         setContentView(R.layout.activity_main);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-    }
 
-    @Override
-    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-        MainActivity.this.mapboxMap = mapboxMap;
-        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+        mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onStyleLoaded(@NonNull Style style) {
-                mapboxMap.addOnMapClickListener(MainActivity.this);
-                markerViewManager = new MarkerViewManager(mapView, mapboxMap);
-                buildingPlugin3D(style);
-                addView();
-                addImage(style);
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                MainActivity.this.mapboxMap = mapboxMap;
+
+                mapboxMap.setStyle(new Style.Builder()
+                                .fromUri(Style.DARK)
+                                .withSource(new GeoJsonSource(DISTANCE_SOURCE_ID))
+                                .withLayer(new LineLayer(DISTANCE_LINE_LAYER_ID, DISTANCE_SOURCE_ID).withProperties(
+                                        lineColor(LINE_COLOR),
+                                        lineWidth(LINE_WIDTH),
+                                        lineJoin(LINE_JOIN_ROUND))), new Style.OnStyleLoaded() {
+                            @Override
+                            public void onStyleLoaded(@NonNull Style style) {
+                                enableLocationComponent(style);
+
+                                MainActivity.this.mapboxMap
+                                        .addOnCameraMoveListener(MainActivity.this);
+
+                                distanceTextView = findViewById(R.id.straight_line_distance_textview);
+                                distanceTextView.setTextColor(Color.RED);
+
+                                Toast.makeText(MainActivity.this,
+                                        getString(R.string.move_map_around_instruction), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
             }
         });
     }
 
-
-    private void addImage(Style style) {
-        LatLngQuad quad = new LatLngQuad(new LatLng(25.7836, 80.11725),
-                new LatLng(25.783548, 80.1397431334),
-                new LatLng(25.7680, 80.13964),
-                new LatLng(25.76795, 80.11725));
-        style.addSource(new ImageSource(ID_IMAGE_SOURCE, quad, R.drawable.logo));
-        style.addLayer(new RasterLayer(ID_IMAGE_LAYER, ID_IMAGE_SOURCE));
+    @Override
+    public void onCameraMove() {
+        redrawLine(mapboxMap.getCameraPosition().target);
     }
 
-    private void addView() {
-        View customView = LayoutInflater.from(MainActivity.this).inflate(R.layout.marker_view_bubble, null);
-        customView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        Button btn = customView.findViewById(R.id.btn);
-        btn.setText("PiraGo");
-        btn.setTextColor(getColor(R.color.colorPrimaryDark));
-        Button btn1 = customView.findViewById(R.id.btn1);
-        btn1.setText("Image");
-        btn1.setTextColor(getColor(R.color.colorPrimaryDark));
-        markerView = new MarkerView(new LatLng(20.996280, 105.826810), customView);
-        markerViewManager.addMarker(markerView);
-        mapboxMap.addMarker(new MarkerOptions().position(new LatLng(20.996280, 105.826810))).setTitle("PiraGo");
-        btn.setOnClickListener(this);
-        btn1.setOnClickListener(this);
-    }
-
-    private void buildingPlugin3D(Style style) {
-        buildingPlugin = new BuildingPlugin(mapView, mapboxMap, style);
-        buildingPlugin.setMinZoomLevel(15f);
-        buildingPlugin.setVisibility(true);
-    }
-
-    private void startSnapShot(final LatLngBounds latLngBounds, final int height, final int width) {
+    private void redrawLine(@NonNull LatLng targetLatLng) {
         mapboxMap.getStyle(new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
-                if (mapSnapshotter == null) {
-                    MapSnapshotter.Options options =
-                            new MapSnapshotter.Options(width, height)
-                                    .withCameraPosition(mapboxMap.getCameraPosition())
-                                    .withStyle(style.getUri())
-                                    .withRegion(latLngBounds);
+                GeoJsonSource geoJsonSource = style.getSourceAs(DISTANCE_SOURCE_ID);
+                if (geoJsonSource != null) {
+                    pointList = new ArrayList<>();
+                    Location lastKnownLocation = mapboxMap.getLocationComponent().getLastKnownLocation();
+                    if (lastKnownLocation != null) {
 
-                    mapSnapshotter = new MapSnapshotter(MainActivity.this, options);
-                } else {
-                    mapSnapshotter.setSize(width, height);
-                    mapSnapshotter.setRegion(latLngBounds);
-                }
-                mapSnapshotter.start(new MapSnapshotter.SnapshotReadyCallback() {
-                    @Override
-                    public void onSnapshotReady(MapSnapshot snapshot) {
-                        createNotification(snapshot.getBitmap());
+// Add the start and ending points to the straight line
+                        Point targetPoint = Point.fromLngLat(targetLatLng.getLongitude(), targetLatLng.getLatitude());
+                        Point deviceLocationPoint = Point.fromLngLat(lastKnownLocation.getLongitude(),
+                                lastKnownLocation.getLatitude());
+                        pointList.add(targetPoint);
+                        pointList.add(deviceLocationPoint);
+
+// Update the source with the new LineString line
+                        geoJsonSource.setGeoJson(LineString.fromLngLats(pointList));
+
+// Update the TextView with the new straight line distance
+                        double distanceBetweenDeviceAndTarget = TurfMeasurement.distance(deviceLocationPoint,
+                                targetPoint, UNIT_METERS);
+
+                        distanceTextView.setText(String.format("%s meters", String.valueOf(
+                                NumberFormat.getNumberInstance(Locale.US).format(distanceBetweenDeviceAndTarget))));
                     }
-                });
+                }
             }
         });
     }
 
-    private void createNotification(Bitmap bitmap) {
-        final int notifyId = 1002;
-        String id = "channel_id";
-        if (notificationManager == null) {
-            notificationManager =
-                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    @SuppressWarnings({"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+// Get an instance of the component
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+// Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+
+// Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+// Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+// Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (notificationManager != null) {
-                NotificationChannel notificationChannel = notificationManager.getNotificationChannel(id);
-                if (notificationChannel == null) {
-                    notificationChannel = new NotificationChannel(id, "channel_name", NotificationManager.IMPORTANCE_HIGH);
-                    notificationChannel.setDescription("channel_description");
-                    notificationManager.createNotificationChannel(notificationChannel);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
                 }
-            }
-        }
-        Intent intent = new Intent(this, MainActivity.class)
-                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, id)
-                .setContentTitle("content")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(("title"))
-                .setContentText(("description"))
-                .setContentIntent(getActivity(this, 0, intent, 0))
-                .setLargeIcon(bitmap);
-        Notification notification = builder.build();
-        notificationManager.notify(notifyId, notification);
-    }
-
-    @Override
-    public boolean onMapClick(@NonNull LatLng point) {
-        startSnapShot(mapboxMap.getProjection().getVisibleRegion().latLngBounds, mapView.getMeasuredHeight(), mapView.getMeasuredWidth());
-        return true;
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn:
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(20.996280, 105.826810)).zoom(10).tilt(20).build();
-                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                break;
-            case R.id.btn1:
-                CameraPosition cameraPosition1 = new CameraPosition.Builder().target(new LatLng(25.783548, 80.1397431334)).zoom(10).tilt(20).build();
-                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition1));
-                break;
+            });
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
         }
     }
 
+    // Add the mapView lifecycle to the activity's lifecycle methods
     @Override
     public void onResume() {
         super.onResume();
@@ -241,6 +219,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (mapboxMap != null) {
+            mapboxMap.removeOnCameraMoveListener(this);
+        }
         mapView.onDestroy();
     }
 
